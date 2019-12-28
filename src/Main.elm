98e56789fb8e399001712b266245c1c4828d8c7e
2mol <2mol@users.noreply.github.com>
2019-12-28 16:23:02 +0100

    canvas wrapper to display background

--- src/Main.elm
+++ src/Main.elm
@@ -1,21 +1,77 @@
 module Main exposing (main)
 
+import List.Extra as List
 import Svg exposing (Svg)
 import Svg.Attributes exposing (..)
 
 
+puzzle =
+    { draftMode = True }
+
+
 main : Svg msg
 main =
+    canvas 800 600 []
+
+
+canvas : Int -> Int -> List (Svg msg) -> Svg msg
+canvas w h children =
+    let
+        hStr =
+            String.fromInt h
+
+        wStr =
+            String.fromInt w
+
+        tileSize =
+            10
+
+        xnumtiles =
+            w // tileSize
+
+        ynumtiles =
+            h // tileSize
+
+        tiles =
+            List.lift2 (tile tileSize)
+                (List.range 0 <| xnumtiles - 1)
+                (List.range 0 <| ynumtiles - 1)
+
+        border =
+            Svg.rect
+                [ x "0", y "0", width wStr, height hStr, fillOpacity "0", stroke "#666" ]
+                []
+
+        maybeTiles =
+            if puzzle.draftMode then
+                [ Svg.g [] tiles, border ]
+
+            else
+                []
+    in
     Svg.svg
-        [ width "800"
-        , height "600"
-        , viewBox "0 0 800 600"
+        [ width wStr
+        , height hStr
+        , viewBox <| "0 0 " ++ wStr ++ " " ++ hStr
         ]
-        [ Svg.rect
-            [ x "0"
-            , y "0"
-            , width "100"
-            , height "100"
-            ]
-            []
+        (maybeTiles ++ [ Svg.g [] children ])
+
+
+tile : Int -> Int -> Int -> Svg msg
+tile size xc yc =
+    let
+        col =
+            if modBy 2 (xc + yc) == 0 then
+                "#eee"
+
+            else
+                "#fff"
+    in
+    Svg.rect
+        [ x (String.fromInt (xc * size))
+        , y (String.fromInt (yc * size))
+        , width (String.fromInt size)
+        , height (String.fromInt size)
+        , fill col
         ]
+        []
